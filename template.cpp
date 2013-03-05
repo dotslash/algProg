78e00da974427bb4276cebe6eea43acb6887b291
dotslash <pratap130492@gmail.com>
2013-03-06 00:17:34 +0530

    c++ template

---
+++ template.cpp
@@ -0,0 +1,41 @@
+#include <cstring>
+#include <cstdio>
+#include <cstdlib>
+#include <climits>
+#include <cmath>
+
+#include <string>
+#include <iostream>
+
+#include <algorithm>
+#include <set>
+#include <map>
+#include <vector>
+
+using namespace std;
+
+#define DEBUG false
+#define MAXV 500010
+#define LL long long
+//for loops
+
+#define rep(i,s,n) for(LL (i)=(s);(i)<(LL)(n);(i)++)
+#define repr(i,s,n) for(LL (i)=(s);(i)>(LL)(n);(i)--)
+#define snuke(c,itr) for(__typeof((c).begin()) itr=(c).begin();itr!=(c).end();itr++)
+
+//vector
+#define vl vector<LL>
+#define vi vector<int>
+#define pb push_back
+//pair
+#define pll pair<LL,LL> 
+#define pii pair<int,int> 
+#define fir first
+#define sec second
+
+
+int main(){
+		
+	return 0;
+}
+
